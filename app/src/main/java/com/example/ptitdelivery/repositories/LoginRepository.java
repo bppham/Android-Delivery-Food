d30ae059b0c3b70c3ff5997e67bfce48ad65e1ae
bppham <phubao180803@gmail.com>
2025-04-24 19:22:09 +0700

    add: some UI activities

---
+++ app/src/main/java/com/example/ptitdelivery/repositories/LoginRepository.java
@@ -0,0 +1,44 @@
+package com.example.ptitdelivery.repositories;
+
+import androidx.lifecycle.MutableLiveData;
+
+import com.example.ptitdelivery.model.Login.LoginRequest;
+import com.example.ptitdelivery.model.Login.LoginResponse;
+import com.example.ptitdelivery.network.ApiClient;
+import com.example.ptitdelivery.network.service.AuthService;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class AuthRepository {
+    private final AuthService authService;
+
+    public AuthRepository(String token) {
+        this.authService = ApiClient.getClient(token).create(AuthService.class);
+    }
+
+    public void login(String email, String password, LoginCallback callback){
+        LoginRequest request = new LoginRequest(email, password);
+        authService.login(request).enqueue(new Callback<LoginResponse>() {
+            @Override
+            public void onResponse(Call<LoginResponse> call, Response<LoginResponse> response) {
+                if (response.isSuccessful() && response.body() != null) {
+                    callback.onSuccess(response.body());
+                } else {
+                    callback.onFailure("Login failed!");
+                }
+            }
+            @Override
+            public void onFailure(Call<LoginResponse> call, Throwable t) {
+                callback.onFailure(t.getMessage());
+            }
+        });
+    }
+    public interface LoginCallback {
+        void onSuccess(LoginResponse loginResponse);
+        void onFailure(String errorMessage);
+    }
+
+
+}
---
+++ app/src/main/java/com/example/ptitdelivery/viewmodel/LoginViewModel.java
@@ -0,0 +1,4 @@
+package com.example.ptitdelivery.viewmodel;
+
+public class LoginViewModel {
+}
